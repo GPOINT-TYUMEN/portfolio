b239c06621c92c11d9f9683f2bd8587056e6d67b
kendrive <new1@mail.ru>
2013-04-01 11:11:57 +0600

    comelook

---
+++ Comelook/js/script.js
@@ -0,0 +1,8 @@
+//$(document).ready(function(){
+//    Cufon.set('fontFamily','DaxlinePro-Medium');
+//    Cufon.replace('.colum-left h2');
+//    Cufon.replace('.block-view-public .title h2');
+//    Cufon.set('fontFamily','Myriad Pro');
+//    Cufon.replace('.colum-left h3');
+//    Cufon.replace('.colum-right h3');
+//});
